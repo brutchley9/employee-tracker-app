540ae8eedb86db9b4d5c6c360f46d57f214ded53
brutchley <bhutchison0901@yahoo.com>
2023-12-09 21:58:29 -0600

    have added seeds.sql values to populate tables with after 'schema.sql' is run with mysql. Still having issues however with manager_id and I may not have syntax correct here.

--- db/schema.sql
+++ db/schema.sql
@@ -1,40 +1,32 @@
---removes database and all entries if it already exists, then creates it again
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
 
---all necessary tables below
-
 CREATE TABLE department (
-    --creates primary key for 'department' to be referenced in 'role'
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    --creates primary key for 'role' to be referenced in 'employee'
+CREATE TABLE task (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    --records salary with length of 9 total possible integers (including decimals), and goes to 2 decimal places
     salary DECIMAL(9, 2),
-    --creating foreign key connection to department primary key called "department_id"
+    department_id INT,
     FOREIGN KEY (department_id)
-    --this below is where it references the id from "department" itself
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    --creates primary key for 'employee' to be referenced by manager_id within same table
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    task_id INT,
+    FOREIGN KEY (task_id)
+    REFERENCES task(id)
     ON DELETE SET NULL,
-
+    manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,8 +4,22 @@ VALUES ("Engineering"),
        ("Legal"),
        ("Sales");
 
-/*INSERT INTO role (department_name)
-VALUES ("Engineering"),
-       ("Finance"),
-       ("Legal"),
-       ("Sales");
+INSERT INTO task (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 2),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 250000, 3),
+       ("Lawyer", 190000, 3);
+
+INSERT INTO employee (first_name, last_name, task_id, manager_id)
+VALUES ("John", "Doe", 4),
+       ("Mike", "Chan", 4, 1),
+       ("Ashley", "Rodriguez", 1),
+       ("Kevin", "Tupik", 1, 3),
+       ("Kunal", "Singh", 2),
+       ("Malia", "Brown", 2, 5),
+       ("Sarah", "Lourd", 3),
+       ("Tom", "Allen", 3, 7);
